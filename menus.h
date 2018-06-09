2f754c10b11d493ca85435bfc9a70a055d5eea65
Vinicius <viniciuacarvalhossp@gmail.com>
2018-06-09 20:04:26 -0300

    Casos de vitoria/derrota

--- menus.h
+++ menus.h
@@ -10,33 +10,60 @@ void menuCategoria(){
 	cout << endl;
 	cout << "0 - BEBIDAS                       1 - CARNES" << endl;
 	cout << "2 - FRIOS                         3 - HORTIFRUTI" << endl;
-	cout << "4 - LIMPEZA                       5 - PADARIA" << endl;
-	cout << endl;
-	cout << "6 para sair do jogo" << endl;
+	cout << "4 - LIMPEZA                       5 - PADARIA" << endl << endl;
+	cout << "6 - Voltar para a casa" << endl;
 
 	cin >> input;
 	switch(input){
 		case 0:
+			if ((errados+certos)>=15){
+				cout << "--" << endl;
+			}
 			refresh();
 /*			menuBEBIDAS();
 		case 1:
+			if ((errados+certos)>=15){
+					cout << "--" << endl;
+			}
 			refresh();
 			menuCARNES();
 		case 2:
+			if ((errados+certos)>=15){
+						cout << "--" << endl;
+			}
 			refresh();
 			menuFRIOS();
 		case 3:
+			if ((errados+certos)>=15){
+						cout << "--" << endl;
+			}		
 			refresh();
 			menuHORTIFRUTI();
 		case 4:
+			if ((errados+certos)>=15){
+						cout << "--" << endl;
+			}		
 			refresh();
 			menuLIMPEZA();
 		case 5:
+			if ((errados+certos)>=15){
+						cout << "--" << endl;
+			}		
 			refresh();
 			menuPADARIA();
 		case 6:
 			refresh();
-			cout << "end game" << endl;
+			if(certos == 10 && errados == 0){
+				cout << "Você voltou pra casa e sua mãe lhe disse que você não fez mais que sua obrigação << endl;
+			}else if (certos == 10 && errados <= 3 ){
+				cout << "Você voltou pra casa e sua mãe ficou furiosa por ter comprado itens a mais" << endl;
+			}else if ((errados+certos)>=15){
+				cout << "--" << endl;
+			}else if ((errados+certos)<10){
+				cout << "Você voltou pra casa e sua mãe ficou furiosa" << endl;
+			}else {
+				cout << "Parabéns, você atingiu um final que os programadores não pensaram" << endl;
+			}
 		default:
 			refresh();
 			menuCategoria();*/
