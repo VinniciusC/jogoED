4d0be148e98199762da585c366507efcc42c2bbd
Vinicius <viniciuacarvalhossp@gmail.com>
2018-06-09 15:56:24 -0300

    Header menus adicionado

--- jogo.cpp
+++ jogo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "ListaProdutos.h"
 #include "CadastraProdutos.h"
+#include "menus.h"
 #ifdef __cplusplus__
   #include <cstdlib>
 #else
@@ -12,47 +13,6 @@ using namespace std;
 void refresh(){
 	if (system("CLS")) system("clear");
 }
-
-void menuCategoria(){
-    refresh();
-	int input;
-	cout << "Voce esta no mercado, navegue nos diferentes setores digitando o numero dele" << endl;
-	cout << endl;
-	cout << "0 - BEBIDAS                       1 - CARNES" << endl;
-	cout << "2 - FRIOS                         3 - HORTIFRUTI" << endl;
-	cout << "4 - LIMPEZA                       5 - PADARIA" << endl;
-	cout << endl;
-	cout << "6 para sair do jogo" << endl;
-
-	cin >> input;
-	switch(input){
-		case 0:
-			refresh();
-/*			menuBEBIDAS();
-		case 1:
-			refresh();
-			menuCARNES();
-		case 2:
-			refresh();
-			menuFRIOS();
-		case 3:
-			refresh();
-			menuHORTIFRUTI();
-		case 4:
-			refresh();
-			menuLIMPEZA();
-		case 5:
-			refresh();
-			menuPADARIA();
-		case 6:
-			refresh();
-			cout << "end game" << endl;
-		default:
-			refresh();
-			menuCategoria();*/
-	}
-}
-
 
 int prefacio(){
     //Tela 1 - Prefácio
---
+++ menus.h
@@ -0,0 +1,41 @@
+#include <iostream>
+void menuCategoria(){
+    refresh();
+	int input;
+	cout << "Voce esta no mercado, navegue nos diferentes setores digitando o numero dele" << endl;
+	cout << endl;
+	cout << "0 - BEBIDAS                       1 - CARNES" << endl;
+	cout << "2 - FRIOS                         3 - HORTIFRUTI" << endl;
+	cout << "4 - LIMPEZA                       5 - PADARIA" << endl;
+	cout << endl;
+	cout << "6 para sair do jogo" << endl;
+
+	cin >> input;
+	switch(input){
+		case 0:
+			refresh();
+/*			menuBEBIDAS();
+		case 1:
+			refresh();
+			menuCARNES();
+		case 2:
+			refresh();
+			menuFRIOS();
+		case 3:
+			refresh();
+			menuHORTIFRUTI();
+		case 4:
+			refresh();
+			menuLIMPEZA();
+		case 5:
+			refresh();
+			menuPADARIA();
+		case 6:
+			refresh();
+			cout << "end game" << endl;
+		default:
+			refresh();
+			menuCategoria();*/
+	}
+}
+
