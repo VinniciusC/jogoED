ec31eb5f33c4e20a40dfdc9c21fd17a42c494056
Vinicius <viniciuacarvalhossp@gmail.com>
2018-06-09 01:14:32 -0300

    Produtos metade para frente adicionados

--- cadastra_produtos.cpp
+++ cadastra_produtos.cpp
@@ -1,6 +1,185 @@
 void cadastraProdutos(Produto* produtos)
 {
-    produtos[0].setID(2);
-    produtos[0].setNome("dsaihsa");
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    produtos[32].setID(32);
+    produtos[32].setNome("Manga");
+
+    produtos[33].setID(33);
+    produtos[33].setNome("Tomate");
+
+    produtos[34].setID(34);
+    produtos[34].setNome("Beringela");
+
+    produtos[35].setID(35);
+    produtos[35].setNome("Beterraba");
+
+    produtos[36].setID(36);
+    produtos[36].setNome("Repolho");
+
+    produtos[37].setID(37);
+    produtos[37].setNome("Vagem");
+
+    produtos[38].setID(38);
+    produtos[38].setNome("Alho");
+
+    produtos[39].setID(39);
+    produtos[39].setNome("Cebola");
+
+    produtos[40].setID(40);
+    produtos[40].setNome("Banana");
+
+    produtos[41].setID(41);
+    produtos[41].setNome("Batata");
+
+    produtos[42].setID(42);
+    produtos[42].setNome("Saco de Lixo");
+
+    produtos[43].setID(43);
+    produtos[43].setNome("Lava Roupas");
+
+    produtos[44].setID(44);
+    produtos[44].setNome("Detergente");
+
+    produtos[45].setID(45);
+    produtos[45].setNome("Alcool");
+
+    produtos[46].setID(46);
+    produtos[46].setNome("Passa Roupas");
+
+    produtos[47].setID(47);
+    produtos[47].setNome("Alvejante");
+
+    produtos[48].setID(48);
+    produtos[48].setNome("Rodo");
+
+    produtos[49].setID(49);
+    produtos[49].setNome("Amaciante");
+
+    produtos[50].setID(50);
+    produtos[50].setNome("Sabao em po");
+
+    produtos[51].setID(51);
+    produtos[51].setNome("Odorizador");
+
+    produtos[52].setID(512);
+    produtos[52].setNome("Torrada");
+
+    produtos[53].setID(53);
+    produtos[53].setNome("Bolacha");
+
+    produtos[54].setID(54);
+    produtos[54].setNome("Pao Sirio");
+
+    produtos[55].setID(55);
+    produtos[55].setNome("Pao Integral");
+
+    produtos[56].setID(56);
+    produtos[56].setNome("Bisnaga");
+
+    produtos[57].setID(57);
+    produtos[57].setNome("Bolo");
+
+    produtos[58].setID(58);
+    produtos[58].setNome("Pao de Mel");
+
+    produtos[59].setID(59);
+    produtos[59].setNome("Pao de Forma");
+
+    produtos[60].setID(60);
+    produtos[60].setNome("Pao de Hot Dog");
+
+    produtos[61].setID(601);
+    produtos[61].setNome("Cocada");
+
+
+
+
