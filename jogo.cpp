d4d04778cad44ba076c5ac264fcad995859273b8
Vinicius <viniciuacarvalhossp@gmail.com>
2018-06-09 15:43:48 -0300

    Menu de categorias adicionado

--- jogo.cpp
+++ jogo.cpp
@@ -13,6 +13,47 @@ void refresh(){
 	if (system("CLS")) system("clear");
 }
 
+void menuCategoria(){
+    refresh();
+	int input;
+	cout << "Voce esta no mercado, navegue nos diferentes setores digitando o numero dele" << endl;
+	cout << endl;
+	cout << "0 - BEBIDAS                       1 - CARNES" << endl;
+	cout << "2 - FRIOS                         3 - HORTIFRUTI" << endl;
+	cout << "4 - LIMPEZA                       5 - PADARIA" << endl;
+	cout << endl;
+	cout << "6 para sair do jogo" << endl;
+
+	cin >> input;
+	switch(input){
+		case 0:
+			refresh();
+/*			menuBEBIDAS();
+		case 1:
+			refresh();
+			menuCARNES();
+		case 2:
+			refresh();
+			menuFRIOS();
+		case 3:
+			refresh();
+			menuHORTIFRUTI();
+		case 4:
+			refresh();
+			menuLIMPEZA();
+		case 5:
+			refresh();
+			menuPADARIA();
+		case 6:
+			refresh();
+			cout << "end game" << endl;
+		default:
+			refresh();
+			menuCategoria();*/
+	}
+}
+
+
 int prefacio(){
     //Tela 1 - Prefácio
     int opcao = 1;
@@ -60,6 +101,12 @@ int main(){
 
     if(opcao == 0)
         opcao = prefacio_2();
+
+    if(opcao == 0){
+        menuCategoria();
+    }
+
+
 
 
 
