f9524639e1c448a5eb3a0bb4d8a3af5d4b747496
Vinicius <viniciuacarvalhossp@gmail.com>
2018-06-09 01:28:12 -0300

    Produtos cadastrados

--- cadastra_produtos.cpp
+++ cadastra_produtos.cpp
@@ -1,142 +1,142 @@
 void cadastraProdutos(Produto* produtos)
 {
+    produtos[0].setID(0);
+    produtos[0].setNome("Refrigerante");
 
+    produtos[1].setID(1);
+    produtos[1].setNome("Corote");
 
+    produtos[2].setID(2);
+    produtos[2].setNome("Vinho");
 
+    produtos[3].setID(3);
+    produtos[3].setNome("Suco em po");
 
+    produtos[4].setID(4);
+    produtos[4].setNome("Cafe em po");
 
+    produtos[5].setID(5);
+    produtos[5].setNome("Suco");
 
+    produtos[6].setID(6);
+    produtos[6].setNome("Vodka");
 
+    produtos[7].setID(7);
+    produtos[7].setNome("Cerveja");
 
+    produtos[8].setID(8);
+    produtos[8].setNome("Energetico");
 
+    produtos[9].setID(9);
+    produtos[9].setNome("Coxa de frango");
 
+    produtos[10].setID(10);
+    produtos[10].setNome("Peito de frango");
 
+    produtos[11].setID(11);
+    produtos[11].setNome("Salmao");
 
+    produtos[12].setID(12);
+    produtos[13].setNome("Coxao mole");
 
+    produtos[14].setID(13);
+    produtos[14].setNome("Lombo suino");
 
+    produtos[15].setID(14);
+    produtos[15].setNome("Bacalhau");
 
+    produtos[16].setID(15);
+    produtos[16].setNome("Contra file");
 
+    produtos[17].setID(16);
+    produtos[17].setNome("Costela bovina");
 
+    produtos[18].setID(17);
+    produtos[18].setNome("Costela suina");
 
+    produtos[19].setID(18);
+    produtos[19].setNome("Camarao");
 
+    produtos[20].setID(19);
+    produtos[20].setNome("Cheddar");
 
+    produtos[21].setID(20);
+    produtos[21].setNome("Mussarela");
 
+    produtos[22].setID(21);
+    produtos[22].setNome("Queijo ralado");
 
+    produtos[23].setID(22);
+    produtos[23].setNome("Queijo prato");
 
+    produtos[24].setID(23);
+    produtos[24].setNome("Linguiça");
 
+    produtos[25].setID(24);
+    produtos[25].setNome("Queijo minas");
 
+    produtos[26].setID(25);
+    produtos[26].setNome("Salame");
 
+    produtos[27].setID(26);
+    produtos[27].setNome("Mortadela");
 
+    produtos[28].setID(27);
+    produtos[28].setNome("Presunto");
 
+    produtos[29].setID(28);
+    produtos[29].setNome("Salsicha");
 
+    produtos[30].setID(29);
+    produtos[30].setNome("Costela Suina");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    produtos[31].setID(31);
+    produtos[31].setNome("Manga");
 
     produtos[32].setID(32);
-    produtos[32].setNome("Manga");
+    produtos[32].setNome("Tomate");
 
     produtos[33].setID(33);
-    produtos[33].setNome("Tomate");
+    produtos[33].setNome("Beringela");
 
     produtos[34].setID(34);
-    produtos[34].setNome("Beringela");
+    produtos[34].setNome("Beterraba");
 
     produtos[35].setID(35);
-    produtos[35].setNome("Beterraba");
+    produtos[35].setNome("Repolho");
 
     produtos[36].setID(36);
-    produtos[36].setNome("Repolho");
+    produtos[36].setNome("Vagem");
 
     produtos[37].setID(37);
-    produtos[37].setNome("Vagem");
+    produtos[37].setNome("Alho");
 
     produtos[38].setID(38);
-    produtos[38].setNome("Alho");
+    produtos[38].setNome("Cebola");
 
     produtos[39].setID(39);
-    produtos[39].setNome("Cebola");
+    produtos[39].setNome("Banana");
 
     produtos[40].setID(40);
-    produtos[40].setNome("Banana");
+    produtos[40].setNome("Batata");
 
     produtos[41].setID(41);
-    produtos[41].setNome("Batata");
+    produtos[41].setNome("Saco de Lixo");
 
     produtos[42].setID(42);
-    produtos[42].setNome("Saco de Lixo");
+    produtos[42].setNome("Lava Roupas");
 
     produtos[43].setID(43);
-    produtos[43].setNome("Lava Roupas");
+    produtos[43].setNome("Detergente");
 
     produtos[44].setID(44);
-    produtos[44].setNome("Detergente");
+    produtos[44].setNome("Alcool");
 
     produtos[45].setID(45);
-    produtos[45].setNome("Alcool");
+    produtos[45].setNome("Passa Roupas");
 
     produtos[46].setID(46);
-    produtos[46].setNome("Passa Roupas");
-
-    produtos[47].setID(47);
-    produtos[47].setNome("Alvejante");
+    produtos[46].setNome("Alvejante");
 
     produtos[48].setID(48);
     produtos[48].setNome("Rodo");
